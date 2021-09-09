51f2e31f197a3c64c1731b97621135279014b658
lue <lue.the.bird@gmail.com>
2021-09-09 22:23:22 +0200

    + review

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,74 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import Documentation.ReadmeLinksPointToCurrentVersion
+import NoBooleanCase
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoForbiddenWords
+import NoImportingEverything
+import NoLeftPizza
+import NoMissingTypeAnnotation
+import NoMissingTypeExpose
+import NoSinglePatternCase
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import OnlyAllSingleUseTypeVarsEndWith_
+import Review.Rule as Rule exposing (Rule)
+import Simplify
+import NoRecordAliasConstructor
+import LinksPointToExistingPackageMembers
+
+
+config : List Rule
+config =
+    [ Documentation.ReadmeLinksPointToCurrentVersion.rule
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoExposingEverything.rule
+    , NoForbiddenWords.rule [ "REPLACEME", "TODO", "todo" ]
+    , NoImportingEverything.rule [ "Nats" ]
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeExpose.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    , Simplify.rule Simplify.defaults
+    , OnlyAllSingleUseTypeVarsEndWith_.rule
+    , NoSinglePatternCase.rule
+        (NoSinglePatternCase.fixInArgument
+            |> NoSinglePatternCase.ifAsPatternRequired
+                (NoSinglePatternCase.fixInLetInstead
+                    |> NoSinglePatternCase.andIfNoLetExists
+                        NoSinglePatternCase.createNewLet
+                )
+        )
+    , NoLeftPizza.rule NoLeftPizza.Any
+    , NoBooleanCase.rule
+    , OnlyAllSingleUseTypeVarsEndWith_.rule
+    , NoRecordAliasConstructor.rule
+    , LinksPointToExistingPackageMembers.rule
+    ]
