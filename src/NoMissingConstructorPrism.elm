8e2afffb216dd85dc6ea1b0c62f79a954b83e7b2
Austin Erlandson <austinerlandson77@gmail.com>
2022-05-13 07:21:46 -0500

    Adds elm-review --fix code generators for generating 'Optional's for Constructors of a Sum type.

---
+++ src/NoMissingConstructorPrism.elm
@@ -0,0 +1,311 @@
+module NoMissingConstructorPrism exposing (rule)
+
+{-|
+
+@docs rule
+
+-}
+
+import Dict
+import Elm.CodeGen as CodeGen exposing (TypeAnnotation)
+import Elm.Pretty exposing (prettyDeclaration)
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Node as Node exposing (Node(..))
+import Elm.Syntax.Type exposing (Type, ValueConstructor)
+import Pretty exposing (pretty)
+import Review.Fix as Fix
+import Review.Rule as Rule exposing (Error, Rule)
+
+
+{-| Uses the Review.Fix api to generate elm-accessors based Prisms for
+Custom Types that don't alread have one defined.
+Because functions in Elm can't start with a capitol letter OR an underscore `_`
+I've used the prefix `c_` as a way to namespace the generated code.
+
+    config =
+        [ NoMissingConstructorPrism.rule
+        ]
+
+see the tests/ for examples of the sort of code that's generated.
+
+
+## When (not) to enable this rule
+
+This rule is useful when you and you're team are using elm-accessors to mitigate
+boilerplate related to updating potentially deeply nested data.
+
+This rule is not useful if you buy into the assumption that Lenses are "too hard"
+or "aren't readible".
+
+
+## Try it out
+
+You can try this rule out by running the following command:
+
+```bash
+elm-review --rules NoMissingConstructorPrism SomeModule.elm
+```
+
+-}
+rule : Rule
+rule =
+    Rule.newModuleRuleSchema "NoMissingConstructorPrism" ()
+        -- Add your visitors
+        -- |> Rule.fromModuleRuleSchema
+        |> Rule.withDeclarationListVisitor generatePrismFromTypeDecl
+        |> Rule.fromModuleRuleSchema
+
+
+generatePrismFromTypeDecl : List (Node Declaration) -> moduleContext -> ( List (Error {}), moduleContext )
+generatePrismFromTypeDecl nodes ctx =
+    ( List.foldr
+        (\(Node injectionRange node) acc ->
+            case node of
+                FunctionDeclaration fn ->
+                    let
+                        fnName : String
+                        fnName =
+                            fn.declaration
+                                |> Node.value
+                                |> .name
+                                |> Node.value
+
+                        ctorName : String
+                        ctorName =
+                            fnName |> String.dropLeft 2
+                    in
+                    if fnName |> String.startsWith "c_" then
+                        Dict.insert ctorName Nothing acc
+
+                    else
+                        acc
+
+                CustomTypeDeclaration type_ ->
+                    let
+                        isAdt : Bool
+                        isAdt =
+                            -- If custom type only has one constructor then
+                            -- we can generate a Lens.
+                            List.length type_.constructors > 1
+                    in
+                    if isAdt then
+                        List.foldr
+                            (\(Node errorRange ctor) ->
+                                let
+                                    ctorName : String
+                                    ctorName =
+                                        Node.value ctor.name
+                                in
+                                if List.length ctor.arguments > 0 then
+                                    Dict.update ctorName
+                                        (\m ->
+                                            case m of
+                                                Nothing ->
+                                                    Just
+                                                        (Just
+                                                            (Rule.errorWithFix
+                                                                { message = "Generating a `c_" ++ ctorName ++ "` Prism for the type constructor: `" ++ ctorName ++ "`."
+                                                                , details = [ "missing prism for constructor `" ++ ctorName ++ "`" ]
+                                                                }
+                                                                errorRange
+                                                                [ Fix.insertAt
+                                                                    { row = injectionRange.end.row + 1
+                                                                    , column = injectionRange.end.column
+                                                                    }
+                                                                    ("\n\n\n"
+                                                                        ++ printPrism type_ ctor
+                                                                    )
+                                                                ]
+                                                            )
+                                                        )
+
+                                                otherwise ->
+                                                    otherwise
+                                        )
+
+                                else
+                                    identity
+                            )
+                            acc
+                            type_.constructors
+
+                    else
+                        acc
+
+                _ ->
+                    acc
+        )
+        Dict.empty
+        nodes
+        |> Dict.values
+        |> List.filterMap identity
+    , ctx
+    )
+
+
+printPrism : Type -> ValueConstructor -> String
+printPrism t ctor =
+    let
+        typeName : String
+        typeName =
+            Node.value t.name
+
+        generics : List String
+        generics =
+            List.map Node.value t.generics
+
+        ctorName : String
+        ctorName =
+            Node.value ctor.name
+
+        { access, update } =
+            implementation ctor
+
+        fnName : String
+        fnName =
+            "c_" ++ ctorName
+    in
+    CodeGen.funDecl
+        Nothing
+        -- TODO: Upgrade once I figure out a `type alias Optional`
+        (CodeGen.funAnn
+            (CodeGen.typed "Relation"
+                ((ctor.arguments
+                    |> List.map Node.value
+                    |> nested CodeGen.tupleAnn
+                 )
+                    ++ [ CodeGen.typeVar "reachable"
+                       , CodeGen.typeVar "wrap"
+                       ]
+                )
+            )
+            (CodeGen.typed "Relation"
+                [ CodeGen.typed typeName (List.map CodeGen.typeVar generics)
+                , CodeGen.typeVar "reachable"
+                , CodeGen.maybeAnn (CodeGen.typeVar "wrap")
+                ]
+            )
+            |> Just
+        )
+        fnName
+        []
+        -- TODO: Add import Accessors exposing (makeOneToOne) or whatever for any
+        -- modules we're generating code for.
+        (CodeGen.construct "makeOneToN_"
+            [ CodeGen.string fnName
+            , access
+            , update
+            ]
+        )
+        |> prettyDeclaration 100
+        |> pretty 100
+
+
+implementation :
+    ValueConstructor
+    ->
+        { access : CodeGen.Expression
+
+        -- , set : CodeGen.Expression
+        , update : CodeGen.Expression
+        }
+implementation ctor =
+    let
+        ctorName : String
+        ctorName =
+            Node.value ctor.name
+    in
+    { access =
+        CodeGen.lambda
+            [ CodeGen.varPattern "fn"
+            , CodeGen.varPattern "t"
+            ]
+            (CodeGen.caseExpr (CodeGen.val "t")
+                [ ( CodeGen.namedPattern ctorName
+                        (List.indexedMap (\ix _ -> CodeGen.varPattern ("a" ++ String.fromInt ix)) ctor.arguments)
+                  , CodeGen.construct "Just"
+                        [ CodeGen.parens
+                            (CodeGen.construct "fn"
+                                --[ CodeGen.tuple (List.indexedMap (\ix _ -> CodeGen.val ("a" ++ String.fromInt ix)) ctor.arguments)
+                                (ctor.arguments
+                                    |> List.indexedMap (\ix _ -> CodeGen.val ("a" ++ String.fromInt ix))
+                                    |> nested CodeGen.tuple
+                                )
+                            )
+                        ]
+                  )
+                , ( CodeGen.allPattern, CodeGen.val "Nothing" )
+                ]
+            )
+    , update =
+        -- TODO: Maybe generate Settable version instead of just the mapping version?
+        CodeGen.lambda
+            [ CodeGen.varPattern "fn"
+            , CodeGen.varPattern "t"
+            ]
+            (CodeGen.caseExpr (CodeGen.val "t")
+                [ ( CodeGen.namedPattern ctorName
+                        (List.indexedMap (\ix _ -> CodeGen.varPattern ("a" ++ String.fromInt ix)) ctor.arguments)
+                  , if List.length ctor.arguments > 1 then
+                        CodeGen.letExpr
+                            [ CodeGen.letFunction "apply_c"
+                                (CodeGen.varPattern "ctor"
+                                    :: (ctor.arguments
+                                            |> List.indexedMap (\ix _ -> CodeGen.varPattern ("t" ++ String.fromInt ix))
+                                            |> nested CodeGen.tuplePattern
+                                       )
+                                )
+                                (CodeGen.construct "ctor" (ctor.arguments |> List.indexedMap (\ix _ -> CodeGen.val ("t" ++ String.fromInt ix))))
+                            ]
+                            (CodeGen.construct "apply_c"
+                                (CodeGen.fun ctorName
+                                    :: [ CodeGen.parens
+                                            (CodeGen.construct "fn"
+                                                --[ CodeGen.tuple (List.indexedMap (\ix _ -> CodeGen.val ("a" ++ String.fromInt ix)) ctor.arguments)
+                                                (ctor.arguments
+                                                    |> List.indexedMap (\ix _ -> CodeGen.val ("a" ++ String.fromInt ix))
+                                                    |> nested CodeGen.tuple
+                                                )
+                                            )
+                                       ]
+                                )
+                            )
+
+                    else
+                        CodeGen.construct ctorName [ CodeGen.parens (CodeGen.construct "fn" [ CodeGen.val "a0" ]) ]
+                  )
+                , ( CodeGen.namedPattern "otherwise" [], CodeGen.val "otherwise" )
+                ]
+            )
+    }
+
+
+
+-- nonEmpty : List a -> Maybe (List a)
+-- nonEmpty =
+--     nelFromList >> Maybe.map nelToList
+
+
+nelToList : ( a, List a ) -> List a
+nelToList ( first, rest ) =
+    first :: rest
+
+
+nelFromList : List a -> Maybe ( a, List a )
+nelFromList ls =
+    case ls of
+        [] ->
+            Nothing
+
+        hd :: tl ->
+            Just ( hd, tl )
+
+
+nested : (List a -> a) -> List a -> List a
+nested toA tpls =
+    case tpls of
+        one :: two :: rest ->
+            [ toA (one :: nested toA (two :: rest)) ]
+
+        otherwise ->
+            otherwise
---
+++ tests/NoMissingConstructorPrismTest.elm
@@ -0,0 +1,245 @@
+module NoMissingConstructorPrismTest exposing (all)
+
+import NoMissingConstructorPrism exposing (rule)
+import Review.Test
+import Test exposing (Test, describe, test)
+
+
+all : Test
+all =
+    describe "NoMissingConstructorPrism"
+        [ describe "should generate" shouldGenerate
+        , describe "should NOT" dontGenerate
+        ]
+
+
+shouldGenerate : List Test
+shouldGenerate =
+    [ test "Prism for types with more than one type variable and tuple up arguments given to the same constructor" <|
+        \() ->
+            """module A exposing (..)
+
+type Data3 a b c d
+    = Data3_Wat a b c d
+    | Data3_Otherwise
+"""
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error
+                        { message = "Generating a `c_Data3_Wat` Prism for the type constructor: `Data3_Wat`."
+                        , details = [ "missing prism for constructor `Data3_Wat`" ]
+                        , under = "Data3_Wat a b c d"
+                        }
+                        |> Review.Test.whenFixed
+                            """module A exposing (..)
+
+type Data3 a b c d
+    = Data3_Wat a b c d
+    | Data3_Otherwise
+
+
+
+c_Data3_Wat :
+    Relation ( a, ( b, ( c, d ) ) ) reachable wrap
+    -> Relation (Data3 a b c d) reachable (Maybe wrap)
+c_Data3_Wat =
+    makeOneToN
+        "c_Data3_Wat"
+        (\\fn t ->
+            case t of
+                Data3_Wat a0 a1 a2 a3 ->
+                    Just (fn ( a0, ( a1, ( a2, a3 ) ) ))
+
+                _ ->
+                    Nothing
+        )
+        (\\fn t ->
+            case t of
+                Data3_Wat a0 a1 a2 a3 ->
+                    let
+                        apply_c ctor ( t0, ( t1, ( t2, t3 ) ) ) =
+                            ctor t0 t1 t2 t3
+                    in
+                    apply_c Data3_Wat (fn ( a0, ( a1, ( a2, a3 ) ) ))
+
+                otherwise ->
+                    otherwise
+        )"""
+                    ]
+    , test "Prisms for types with more than one type variable" <|
+        \() ->
+            """module A exposing (..)
+
+
+
+type Data2 a b
+    = Data2_Things a
+    | Data2_Blah b
+    | Data2_Otherwise
+"""
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error
+                        { message = "Generating a `c_Data2_Things` Prism for the type constructor: `Data2_Things`."
+                        , details = [ "missing prism for constructor `Data2_Things`" ]
+                        , under = "Data2_Things a"
+                        }
+                        |> Review.Test.whenFixed
+                            """module A exposing (..)
+
+
+
+type Data2 a b
+    = Data2_Things a
+    | Data2_Blah b
+    | Data2_Otherwise
+
+
+
+c_Data2_Things : Relation a reachable wrap -> Relation (Data2 a b) reachable (Maybe wrap)
+c_Data2_Things =
+    makeOneToN
+        "c_Data2_Things"
+        (\\fn t ->
+            case t of
+                Data2_Things a0 ->
+                    Just (fn a0)
+
+                _ ->
+                    Nothing
+        )
+        (\\fn t ->
+            case t of
+                Data2_Things a0 ->
+                    Data2_Things (fn a0)
+
+                otherwise ->
+                    otherwise
+        )"""
+                    , Review.Test.error
+                        { message = "Generating a `c_Data2_Blah` Prism for the type constructor: `Data2_Blah`."
+                        , details = [ "missing prism for constructor `Data2_Blah`" ]
+                        , under = "Data2_Blah b"
+                        }
+                        |> Review.Test.whenFixed
+                            """module A exposing (..)
+
+
+
+type Data2 a b
+    = Data2_Things a
+    | Data2_Blah b
+    | Data2_Otherwise
+
+
+
+c_Data2_Blah : Relation b reachable wrap -> Relation (Data2 a b) reachable (Maybe wrap)
+c_Data2_Blah =
+    makeOneToN
+        "c_Data2_Blah"
+        (\\fn t ->
+            case t of
+                Data2_Blah a0 ->
+                    Just (fn a0)
+
+                _ ->
+                    Nothing
+        )
+        (\\fn t ->
+            case t of
+                Data2_Blah a0 ->
+                    Data2_Blah (fn a0)
+
+                otherwise ->
+                    otherwise
+        )"""
+                    ]
+    , test "a basic prism" <|
+        \() ->
+            """module A exposing (..)
+
+
+type Data1 a
+    = Data1_Stuff a
+    | Data1_Otherwise
+"""
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error
+                        { message = "Generating a `c_Data1_Stuff` Prism for the type constructor: `Data1_Stuff`."
+                        , details = [ "missing prism for constructor `Data1_Stuff`" ]
+                        , under = "Data1_Stuff a"
+                        }
+                        |> Review.Test.whenFixed
+                            """module A exposing (..)
+
+
+type Data1 a
+    = Data1_Stuff a
+    | Data1_Otherwise
+
+
+
+c_Data1_Stuff : Relation a reachable wrap -> Relation (Data1 a) reachable (Maybe wrap)
+c_Data1_Stuff =
+    makeOneToN
+        "c_Data1_Stuff"
+        (\\fn t ->
+            case t of
+                Data1_Stuff a0 ->
+                    Just (fn a0)
+
+                _ ->
+                    Nothing
+        )
+        (\\fn t ->
+            case t of
+                Data1_Stuff a0 ->
+                    Data1_Stuff (fn a0)
+
+                otherwise ->
+                    otherwise
+        )"""
+                    ]
+    ]
+
+
+dontGenerate : List Test
+dontGenerate =
+    [ test "attempt to fix Lens-able types" <|
+        \() ->
+            """module A exposing (..)
+
+
+type NotAVariant =
+    NotAVariant String
+"""
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    , test "generate Prism for constructors that already have a prism defined." <|
+        \() ->
+            """module A exposing (..)
+
+type AlreadyDefined a
+    = DontError a
+    | Whatever
+
+c_DontError : Prism (AlreadyDefined a) a
+c_DontError =
+    makeOneToOne "c_DontError"
+        (\\t ->
+            case t of
+                DontError a -> Just a
+                otherwise -> Nothing
+            )
+        (\\fn t ->
+            case t of
+                DontError a -> DontError (fn a)
+                otherwise -> otherwise
+        )
+
+
+"""
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    ]
