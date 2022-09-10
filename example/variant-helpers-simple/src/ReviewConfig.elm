2f5b0ea1e269c5f55e407da5da675575fe149a4b
lue-bird <lue.the.bird@gmail.com>
2022-09-10 17:20:23 +0200

    examples package dependency → 2.1.0

---
+++ example/variant-helpers-simple/src/ReviewConfig.elm
@@ -0,0 +1,79 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import Dict
+import Review.Rule as Rule exposing (Rule)
+import VariantHelper.GenerateUsed exposing (VariantHelperNameConfig, VariantHelperBuild, ValuesCombined)
+import Elm.CodeGen as CodeGen
+
+
+config : List Rule
+config =
+    [ VariantHelper.GenerateUsed.rule
+        { nameInModuleInternal = VariantHelper.GenerateUsed.variantAfter "to"
+        , nameInModuleExternal = VariantHelper.GenerateUsed.variantAfter "to"
+        , build =
+            variantAccessTry
+                { valuesCombined = VariantHelper.GenerateUsed.valuesRecord }
+        , generationModuleIsVariantModuleDotSuffix = "Help"
+        }
+    ]
+
+
+
+variantAccessTry :
+    { valuesCombined : ValuesCombined }
+    -> VariantHelperBuild
+variantAccessTry { valuesCombined } =
+    \{ typeName, variantName, variantValues, typeParameters, otherVariants } ->
+        let
+            generation =
+                if otherVariants |> Dict.isEmpty then
+                    VariantHelper.GenerateUsed.variantOnly
+                        { name = variantName
+                        , values = variantValues
+                        , valuesCombined = valuesCombined
+                        }
+                
+                else
+                    VariantHelper.GenerateUsed.variantInMultiple
+                        { name = variantName
+                        , values = variantValues
+                        , valuesCombined = valuesCombined
+                        }
+        in
+        { imports = []
+        , documentation =
+            CodeGen.emptyDocComment
+                |> CodeGen.markdown
+                    ([ "Try to access the values on the variant `"
+                     , typeName
+                     , "."
+                     , variantName
+                     , "`"
+                     ]
+                        |> String.concat
+                    )
+                |> Just
+        , annotation =
+            CodeGen.funAnn
+                (CodeGen.typed typeName
+                    (typeParameters |> List.map CodeGen.typeVar)
+                )
+                (CodeGen.maybeAnn
+                    generation.typeValues
+                )
+                |> Just
+        , implementation =
+            generation.access
+        }
